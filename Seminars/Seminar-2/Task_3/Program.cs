e7c9a1d818530a5cd8f0e7332e790aba03af5def
ServEr <Susurkaev@yandex.ru>
2023-02-22 15:53:29 +0300

    Seminar-2, Task_2.1, Task_3, Task_4, Task_5

---
+++ Seminars/Seminar-2/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток 
+//от деления.
+
+Console.WriteLine("Введите первое число:");
+double a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double b = int.Parse(Console.ReadLine());
+
+//double result = b / a;
+
+//Console.WriteLine(b % a);
+if (a % b == 0)
+ {
+     System.Console.WriteLine("Второе число кратно первому.");
+ }
+ else
+ {
+     //result = result % 10;
+     System.Console.WriteLine("Второе число не кратно первому. Результат от деления: " + a / b);
+}
---
+++ Seminars/Seminar-2/Task_5/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число 
+// квадратом другого.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine("Число b является квадратом числа a");
+}
+else if (b * b == a)
+{
+    Console.WriteLine("Число a является квадратом числа b");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратами друг для друга");
+}
+
