d4988ff7131f886d82214e7fbb68e9f676528906
ServEr <Susurkaev@yandex.ru>
2023-02-23 08:21:40 +0300

    Task_10 code corrected

--- Seminars/Seminar-2/HomeWork/Task_10/Program.cs
+++ Seminars/Seminar-2/HomeWork/Task_10/Program.cs
@@ -1,17 +1,14 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
 //выходе показывает вторую цифру этого числа.
 
-//bool value = true;
 Console.WriteLine("Введите 3-х значное чило: ");
 int number = int.Parse(Console.ReadLine());
 if (number.ToString().Length == 3)
-{
-    //int number = int.Parse(Console.ReadLine());
-    int result = number / 10;
-    result = result % 10;
-    Console.WriteLine(result % 10);
-}
+    {
+        int result = number / 10;
+        Console.WriteLine(result % 10);
+    }
 else
-{
-    Console.WriteLine("Введенное число не является 3-х значным");
-}
+    {
+        Console.WriteLine("Введенное число не является 3-х значным");
+    }
