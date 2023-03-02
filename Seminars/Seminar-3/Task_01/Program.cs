da957f92b2131c4e7efa1437633eb6740d5ddad7
ServEr <Susurkaev@yandex.ru>
2023-03-02 15:05:45 +0300

    Task_01 recoded

--- Seminars/Seminar-3/Task_01/Program.cs
+++ Seminars/Seminar-3/Task_01/Program.cs
@@ -1,24 +1,23 @@
 ﻿//Задача 1.
-// Перемешивание элементов массива
+//Перемешивание элементов массива
 
 Console.WriteLine("Укажите размерность массива");
 int arrayLength = int.Parse(Console.ReadLine());
 int[] array = new int[arrayLength];
 int index = 0;
 
-while (index < arrayLength)
+while (index < array.Length)
 {
     Console.Write("Введите элемент массива: ");
     array[index] = int.Parse(Console.ReadLine());
     index ++;
 }
-
 int oldValue = 0;
 int newValue= 0;
 
-for (int i = 0; i < array.Length - 1; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    newValue = new Random().Next(i + 1, 9);
+    newValue = new Random().Next(i, array.Length + 1);
     
     oldValue = array[i];
     array[i] = array[newValue];
